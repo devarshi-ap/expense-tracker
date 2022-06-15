b27f672f1c5a5168b42267a58371898cf5c08426
Dev Patel <devarshi.ap@gmail.com>
2022-06-15 19:26:05 -0400

    upstream commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./index.html', './src/**/*.{jsx,js}'],
+  theme: {
+    extend: {},
+    colors: {
+      transparent: 'transparent',
+      white: '#ffffff',
+      purple: '#3f3cbb',
+      midnight: '#121063',
+      metal: '#565584',
+      silver: '#ecebff',
+      blue: '#87ceeb'
+    },
+  },
+  plugins: [],
+};
