4e85fe8d1ad022015e0a1bd9c3e6cd407c01277b
Dev Patel <devarshi.ap@gmail.com>
2022-06-15 23:33:53 -0400

    readied form for redux

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -8,10 +8,13 @@ export default function Form() {
     type
     amount
     */
+    let dateObj = new Date(),
+        todaysDate = `${dateObj.getFullYear()}-${dateObj.getMonth() + 1}-${dateObj.getDate()}`;
 
     const [name, setName] = useState('');
     const [category, setCategory] = useState('Bills & Utilities');
     const [type, setType] = useState('Expense');
+    const [date, setDate] = useState(todaysDate);
     const [amount, setAmount] = useState(0);
     
     // https://mint.intuit.com/mint-categories/
@@ -39,9 +42,22 @@ export default function Form() {
         <option key={index} value={category}>{category}</option>
     ))
 
+    const handleSubmit = e => {
+        e.preventDefault();
+
+        // Dispatch Action
+
+        // Refresh State
+        setName('');
+        setCategory('Bills & Utilities');
+        setType('Expense');
+        setDate(todaysDate)
+        setAmount(0);
+    }
+
     return (
         <div className="m-2 block p-7 max-w-lg bg-[#f1f5f9] rounded-lg border border-gray-200 shadow-md dark:border-gray-700 dark:hover:bg-gray-700">
-            <form>
+            <form onSubmit={handleSubmit}>
                 <input
                     className="w-full h-9 mb-1 rounded-sm text-center"
                     type="text" 
@@ -66,6 +82,15 @@ export default function Form() {
                         value={amount}
                         onChange={e => setAmount(e.target.value)}
                         placeholder="Amount ($)"
+                    ></input>
+                </div>
+
+                <div className="mt-2 mb-5">
+                    <input
+                        className="w-full h-9 rounded-sm text-center"
+                        type="date"
+                        value={date}
+                        onChange={e => setDate(e.target.value)}
                     ></input>
                 </div>
                 
