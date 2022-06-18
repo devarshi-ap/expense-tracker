1fc3f09f8e333a4d9727831ae6472a82fd470a9e
Dev Patel <devarshi.ap@gmail.com>
2022-06-18 00:11:32 -0400

    add sort by price feature

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,44 @@
 import Balance from "./components/Balance";
 import Form from './components/Form';
 import TransactionLog from './components/TransactionLog';
+import { BsSortNumericDownAlt, BsSortNumericUpAlt } from 'react-icons/bs'
+import { useState } from "react";
+import { useDispatch } from "react-redux";
+import { sortPriceHighToLow, sortPriceLowToHigh } from "./store/transactionSlice";
 
 export default function App() {
-  return (
+    
+    const [sortPrice, setSortPrice] = useState('high')
+
+    const dispatch = useDispatch();
+
+    const toggleSort = () => {
+        if (sortPrice === 'high') {
+            setSortPrice('low')
+            dispatch(sortPriceLowToHigh())
+        }
+        else if (sortPrice === 'low') {
+            setSortPrice('high')
+            dispatch(sortPriceHighToLow())
+        }
+    }
+
+    return (
     <div className="grid place-content-center bg-[#e0f2fe] min-h-screen py-10">
         <div className="flex flex-col sm:flex-col md:w-[30rem]">
             <div>
                 <Balance />
                 <Form />
             </div>
-            <div className="w-full mb-1 pl-3 py-2 text-xl font-mono font-semibold underline text-metal">Recent Transactions</div>
+            <div className="w-full mb-1 pl-3 py-2 flex justify-between">
+                <div className="w-fit text-xl font-mono font-semibold underline text-metal">Recent Transactions</div>
+                <div>
+                    {sortPrice === 'high' ? 
+                        <BsSortNumericDownAlt className="text-3xl text-metal" onClick={toggleSort}/> :
+                        <BsSortNumericUpAlt className="text-3xl text-metal" onClick={toggleSort}/>
+                    }
+                </div>
+            </div>
             <TransactionLog />
         </div>
     </div>
