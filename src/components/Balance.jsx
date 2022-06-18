e1d6ea52853754f40de62db1cd5ece676f6348d2
Dev Patel <devarshi.ap@gmail.com>
2022-06-18 00:12:05 -0400

    add text color animation

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -6,7 +6,7 @@ export default function Balance() {
 
     return (
         <div className="inline-flex flex-row h-20 w-full justify-between">
-            <h2 className="w-fit ml-3 text-4xl font-mono text-metal">Hi Dev!<br />${balance}</h2>
+            <h2 className="w-fit ml-3 text-4xl font-mono bg-gradient-to-r bg-clip-text  text-transparent  from-metal via-[#c6b3d8] to-metal animate-text">Hi Dev!<br />${balance}</h2>
             <img src="https://i.imgur.com/LS0NftU.gif" className="h-20 w-auto mr-7 top-4 relative rounded-lg" alt="" />
         </div>
     )
