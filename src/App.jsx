f788b8413733c020116d5f82bcfaabdd06e23f3f
Dev Patel <devarshi.ap@gmail.com>
2022-06-20 09:57:48 -0400

    add sort by date

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,19 @@
 import Balance from "./components/Balance";
 import Form from './components/Form';
 import TransactionLog from './components/TransactionLog';
-import { BsSortNumericDownAlt, BsSortNumericUpAlt } from 'react-icons/bs'
+import { BsSortNumericDownAlt, BsSortNumericUpAlt, BsSortDownAlt, BsSortUpAlt } from 'react-icons/bs'
 import { useState } from "react";
 import { useDispatch } from "react-redux";
-import { sortPriceHighToLow, sortPriceLowToHigh } from "./store/transactionSlice";
+import { sortPriceHighToLow, sortPriceLowToHigh, sortDateHighToLow, sortDateLowToHigh } from "./store/transactionSlice";
 
 export default function App() {
     
     const [sortPrice, setSortPrice] = useState('high')
+    const [sortDate, setSortDate] = useState('high')
 
     const dispatch = useDispatch();
 
-    const toggleSort = () => {
+    const togglePriceSort = () => {
         if (sortPrice === 'high') {
             setSortPrice('low')
             dispatch(sortPriceLowToHigh())
@@ -20,6 +21,17 @@ export default function App() {
         else if (sortPrice === 'low') {
             setSortPrice('high')
             dispatch(sortPriceHighToLow())
+        }
+    }
+
+    const toggleDateSort = () => {
+        if (sortDate === 'high') {
+            setSortDate('low')
+            dispatch(sortDateLowToHigh())
+        }
+        else if (sortDate === 'low') {
+            setSortDate('high')
+            dispatch(sortDateHighToLow())
         }
     }
 
@@ -32,10 +44,15 @@ export default function App() {
             </div>
             <div className="w-full mb-1 pl-3 py-2 flex justify-between">
                 <div className="w-fit text-xl font-mono font-semibold underline text-metal">Recent Transactions</div>
-                <div>
+                <div className="inline-flex">
+
                     {sortPrice === 'high' ? 
-                        <BsSortNumericDownAlt className="text-3xl text-metal" onClick={toggleSort}/> :
-                        <BsSortNumericUpAlt className="text-3xl text-metal" onClick={toggleSort}/>
+                        <BsSortNumericDownAlt data-tooltip-target="tooltip-default" className="text-3xl text-metal mx-1" onClick={togglePriceSort}/> : 
+                        <BsSortNumericUpAlt className="text-3xl text-metal mx-1" onClick={togglePriceSort}/>
+                    }
+                    {sortDate === 'high' ? 
+                        <BsSortDownAlt className="text-3xl text-metal mx-1" onClick={toggleDateSort}/> :
+                        <BsSortUpAlt className="text-3xl text-metal mx-1" onClick={toggleDateSort}/>
                     }
                 </div>
             </div>
