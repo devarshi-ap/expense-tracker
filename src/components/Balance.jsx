86a749db704c4dfdb97a27014f3e6fb2f85bb0ba
Dev Patel <devarshi.ap@gmail.com>
2022-06-20 10:26:10 -0400

    change title

--- src/components/Balance.jsx
+++ src/components/Balance.jsx
@@ -6,7 +6,7 @@ export default function Balance() {
 
     return (
         <div className="inline-flex flex-row h-20 w-full justify-between">
-            <h2 className="w-fit ml-3 text-4xl font-mono bg-gradient-to-r bg-clip-text  text-transparent  from-metal via-[#c6b3d8] to-metal animate-text" data-cy="account-balance">Hi Dev!<br />${parseFloat(balance).toFixed(2)}</h2>
+            <h2 className="w-fit ml-3 text-4xl font-mono bg-gradient-to-r bg-clip-text  text-transparent  from-metal via-[#c6b3d8] to-metal animate-text" data-cy="account-balance">Hey Guy!<br />${parseFloat(balance).toFixed(2)}</h2>
             <img src="https://i.imgur.com/LS0NftU.gif" className="h-20 w-auto mr-7 top-4 relative rounded-lg hover:cursor-pointer" alt="" onClick={() => window.location.reload(false)}/>
         </div>
     )
