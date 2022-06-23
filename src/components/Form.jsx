662f4e7ab2215c57c768f5f7506f75696b9e11ab
Dev Patel <devarshi.ap@gmail.com>
2022-06-23 12:59:25 -0400

    add label in form

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -139,13 +139,15 @@ export default function Form() {
                 </div>
 
                 <div className="mt-2 mb-5">
-                    <input
-                        className="w-full h-9 rounded-sm text-center"
-                        type="date"
-                        value={date}
-                        onChange={e => setDate(e.target.value)}
-                        data-cy="txn-date"
-                    ></input>
+                    <label>
+                        <input
+                            className="w-full h-9 rounded-sm text-center"
+                            type="date"
+                            value={date}
+                            onChange={e => setDate(e.target.value)}
+                            data-cy="txn-date"
+                        ></input>
+                    </label>
                 </div>
                 
                 <button
